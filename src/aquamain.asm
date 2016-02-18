19a1b9b182cc7205a1bcb075692fa7d9fd2176aa
Matthew Morra <matt@aquagps.com>
2016-02-18 17:28:29 -0500

    tabs n syntax

--- src/aquamain.asm
+++ src/aquamain.asm
@@ -10,7 +10,7 @@
 #include "brex.inc"
 
 RES_VECT      CODE    0x0000
-  goto        START
+    goto        START
 
 ; TODO ADD INTERRUPTS HERE IF USED
 
@@ -18,15 +18,16 @@ MAIN_PROG CODE
 
 START
 
-  PIC_INIT                          ; Initialize clock, I/O, analog input, WDT, I2C
+    PIC_INIT                          ; Initialize clock, I/O, analog input, WDT, I2C
 
-  call        IS_THIS_FIRST_PWRUP   ; 1 in w = first powerup, 0 in w = not first powerup
-  iorlw       .0                    ; Update Z bit in status flag
-  btfss       STATUS,Z
-  call        FIRST_PWRUP_CODE
-
+    call        IS_THIS_FIRST_PWRUP   ; 1 in w = first powerup, 0 in w = not first powerup
+    iorlw       .0                    ; Update Z bit in status flag
+    btfss       STATUS,Z
+    call        FIRST_PWRUP_CODE      ; w is 1, so this means it is the first powerup. Init the am1805
+                                    ; This line reached if it is not the first powerup, or the first
+                                    ; powerup init code has been executed.
 	call        DOUBLEBLINKLED
-  clrwdt
-	goto	      $-2
+    clrwdt
+	goto	    $-2
 
 	END
