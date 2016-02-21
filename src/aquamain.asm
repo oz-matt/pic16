2a64c9c754f96641ea47ed4583ae51ff329b5e1b
Matthew Morra <matt@aquagps.com>
2016-02-21 00:52:09 -0500

    up to read x40

--- src/aquamain.asm
+++ src/aquamain.asm
@@ -8,6 +8,9 @@
 #include "ichex.inc"
 #include "eeprom.inc"
 #include "brex.inc"
+#include "am1805.inc"
+#include "math.inc"
+#include "eusart.inc"
 
 RES_VECT      CODE    0x0000
     goto        START
@@ -22,10 +25,37 @@ START
 
     call        IS_THIS_FIRST_PWRUP   ; 1 in w = first powerup, 0 in w = not first powerup
     iorlw       .0                    ; Update Z bit in status flag
+    BANKSEL     STATUS
     btfss       STATUS,Z
     call        FIRST_PWRUP_CODE      ; w is 1, so this means it is the first powerup. Init the am1805
                                     ; This line reached if it is not the first powerup, or the first
                                     ; powerup init code has been executed.
+    call        IS_USBON_HIGH
+    iorlw       .0
+    BANKSEL     STATUS
+    btfss       STATUS,Z
+    call        USB_PLUGGED_AT_START  ; w is 1, USB is plugged in
+                                    ; w is 0, so USB is not plugged in
+
+    call        ENSUREPSW_LOW         ; Make sure we don't get shut off by AM1805 inadvertently
+
+    call        IS_TC_HIGH
+    iorlw       .0
+    BANKSEL     STATUS
+    btfss       STATUS,Z
+    call        TC_HIGH               ; w is 1, TC is high, AM1805 just woke us up.
+                                    ; w is 0, TC is low, Main power turn-on just woke us up.
+
+    call        AM1805_ARE_WE_ASLEEP
+    iorlw       .0
+    BANKSEL     STATUS
+    btfss       STATUS,Z
+    call        SLEEP_ERROR           ; w is 1, we are sleeping
+                                    ; w is 0, we are not sleeping
+    call        ALLOW_UPDATE_SD_TO_EEPROM
+
+    call        RESET_INSTRUCTION_PTR
+
 	call        DOUBLEBLINKLED
     clrwdt
 	goto	    $-2
